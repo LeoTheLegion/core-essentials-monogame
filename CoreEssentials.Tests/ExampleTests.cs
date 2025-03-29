900b39a8f3e7aaa11736c76a343c273a333a0eea
LeoTheLegion <halelt@live.com>
2025-03-29 15:30:21 -0400

    Add unit test project and example test for CoreEssentials

---
+++ CoreEssentials.Tests/ExampleTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ExampleTests {
+    [TestMethod]
+    public void TestName() {
+        // ...test logic...
+        Assert.IsTrue(true);
+    }
+}
