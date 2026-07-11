261eb310300191e8470ecd6a5a42b6fd7425ccb9
LeoTheLegion <halelt@live.com>
2026-07-11 16:56:36 -0400

    more xml updates

--- CoreEssentials/src/assets/XMLAsset.cs
+++ CoreEssentials/src/assets/XMLAsset.cs
@@ -2,16 +2,30 @@ using System;
 
 namespace CoreEssentials.Assets;
 
+/// <summary>
+/// Represents an asset containing XML configuration data for game resources.
+/// </summary>
 public class XMLAsset : Asset
 {
-    private string _xmlContent;
+    private string _xmlContent = "";
 
+    /// <summary>
+    /// Gets the raw XML content of this asset.
+    /// </summary>
     public string XMLContent => _xmlContent;
 
+    /// <summary>
+    /// Initializes a new instance of the XMLAsset class with the specified name.
+    /// </summary>
+    /// <param name="name">The name/identifier for this asset.</param>
     public XMLAsset(string name) : base(name)
     {
     }
 
+    /// <summary>
+    /// Loads the XML content from file into this asset instance.
+    /// </summary>
+    /// <param name="contentManager">The content manager used to locate and load assets.</param>
     public override void Load(IContentManager contentManager)
     {
         if (contentManager == null)
